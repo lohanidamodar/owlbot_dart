57da820ccfb6cd782c32c0424dac636d5e7b9edb
Damodar Lohani <dlohani48@gmail.com>
2020-08-31 16:56:27 +0545

    fixes, remove flutter dependency

--- lib/owlbot_dart.dart
+++ lib/owlbot_dart.dart
@@ -1,37 +1,9 @@
 library owlbot_dart;
 
 import 'dart:convert';
-
 import 'package:meta/meta.dart';
 import 'package:http/http.dart' as http;
 
 part 'src/owlbot.dart';
 part 'src/owlbot_res.dart';
 part 'src/owlbot_definition.dart';
-/* function Owlbot(token) {
-    if (!token) {
-      throw "Token is not provided. if you don't have a token, you can get one at https://owlbot.info.";
-    }
-    return {
-        client: axios.create({
-            baseURL: 'https://owlbot.info',
-            timeout: 5000,
-            headers: {'Authorization': 'Token '+token}
-        }),
-        data: null,
-        define: async function(word) {
-            if(!word){
-                throw 'Word is not provided.';
-            }
-            var self = this;
-            await this.client.get('/api/v4/dictionary/'+word).then(function (response) {
-                self.data = response.data;
-            }).catch(function(error){
-                throw error;
-            });
-            return self.data;
-        }
-    }
-}
-
-module.exports = Owlbot; */
--- test/owlbot_dart_test.dart
+++ test/owlbot_dart_test.dart
@@ -1,9 +1,9 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:owlbot_dart/owlbot_dart.dart';
 import 'token.dart';
 
 void main() {
-  test('can get definition', ()async {
+  test('can get definition', () async {
     final owlbot = OwlBot(token: TOKEN);
     final res = await owlbot.define(word: "owl");
     assert(res is OwlBotResponse);
